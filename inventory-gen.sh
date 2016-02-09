f1f1db0c3dd3ad07ee2572d4709886d4a2e894de
Leif Madsen <lmadsen@redhat.com>
2016-02-09 08:47:25 -0500

    Moving forward with inventory cleanup

--- inventory-gen.sh
+++ inventory-gen.sh
@@ -1,15 +1,25 @@
 #!/usr/bin/env bash
-truncate --size 0 bootstrap
-truncate --size 0 new_inventory
-
 bgp_asn=65100
+bootstrap_file=bootstrap
+inventory_file=new_inventory
 
-cat >> bootstrap <<EOL
+truncate --size 0 $bootstrap_file
+truncate --size 0 $inventory_file
+
+
+cat >> $bootstrap_file <<EOL
 [overcloud]
 EOL
 
-for host in {23..27}; do
-	result=`ssh heat-admin@172.16.0.$host "/usr/sbin/ip a s | awk '/inet / {print $2};' | grep '172.16.0' | grep -v '\/32' && hostname -f"`
+cat >> $inventory_file <<EOL
+[overcloud:vars]
+ansible_ssh_user=heat-admin
+
+[overcloud]
+EOL
+
+for host in $(nova list --fields Networks | awk '/ctlplane/ {print $4};' | cut -d'=' -f2); do
+	result=`ssh heat-admin@$host "/usr/sbin/ip a s | awk '/inet / {print $2};' | grep '172.16.0' | grep -v '\/32' && hostname -f"`
 	hostname=`echo $result | awk '/inet / {print $9};'`
 	address=`echo $result | awk '/inet / {print $2};'`
 	ip=`echo $address | cut -f1 -d'/'`
@@ -25,14 +35,14 @@ for host in {23..27}; do
 	new_ip+='.'
 	new_ip+=`echo $ip | cut -f4 -d'.'`
 
-	cat >> bootstrap <<EOL
-$hostname ansible_ssh_host=$ip ansible_ssh_user=heat-admin local_prefix=$prefix local_interface=$interface local_new_ipaddr=$new_ip
+	cat >> $bootstrap_file <<EOL
+$hostname ansible_ssh_host=$ip ansible_ssh_user=heat-admin local_prefix=$prefix local_interface=$interface:1 local_ipaddr=$new_ip
 EOL
 
 
 	bgp_asn=$[$bgp_asn+1]
 
-	cat >> new_inventory <<EOL
-$hostname ansible_ssh_host=$new_ip bgp_asn=$bgp_asn base_interface=$interface
+	cat >> $inventory_file <<EOL
+$hostname ansible_ssh_host=$new_ip bgp_asn=$bgp_asn base_interface=$interface local_ipaddr=$ip local_prefix=32
 EOL
 done
