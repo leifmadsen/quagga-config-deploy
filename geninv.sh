50d4db2ca18a9e47735544cd4d263c316dfecc88
Leif Madsen <lmadsen@redhat.com>
2016-02-12 13:26:19 -0500

    Update syntax to use variables instead of hard coded vars

--- geninv.sh
+++ geninv.sh
@@ -2,6 +2,11 @@
 bgp_asn=65100
 bootstrap_file=bootstrap
 inventory_file=inventory
+default_network="10.17.66"
+network=${1:-$default_network}
+
+echo $network
+exit
 
 truncate --size 0 $bootstrap_file
 truncate --size 0 $inventory_file
@@ -19,7 +24,7 @@ ansible_ssh_user=heat-admin
 EOL
 
 for host in $(nova list --fields Networks | awk '/ctlplane/ {print $4};' | cut -d'=' -f2); do
-	result=`ssh -o StrictHostKeyChecking=no heat-admin@$host "/usr/sbin/ip a s | awk '/inet / {print $2};' | grep '10.17.66' | grep -v '\/32' && hostname -f"`
+	result=`ssh -o StrictHostKeyChecking=no heat-admin@$host "/usr/sbin/ip a s | awk '/inet / {print $2};' | grep '$network' | grep -v '\/32' && hostname -f"`
 	hostname=`echo $result | awk '/inet / {print $9};'`
 	address=`echo $result | awk '/inet / {print $2};'`
 	ip=`echo $address | cut -f1 -d'/'`
